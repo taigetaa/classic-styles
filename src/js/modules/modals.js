2e4c2e960fe633438376d085540baca7a7d7123a
Victoria <bethmorgendorffer@mail.ru>
2023-05-16 12:24:38 +0300

    fixes

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,52 +1,56 @@
 const modals = () => {
-    let bindModal = (triggerSelector, modalSelector, closeSelector) => {
-        const triggers = document.querySelectorAll(triggerSelector),
-              modal = document.querySelector(modalSelector),
-              close = document.querySelector(closeSelector);
+  let bindModal = (triggerSelector, modalSelector, closeSelector) => {
+    const triggers = document.querySelectorAll(triggerSelector);
+    const modal = document.querySelector(modalSelector);
+    const close = document.querySelector(closeSelector);
 
-    let closeModal = () => {
-        modal.style.display = "none";
-        document.body.style.overflow = "";
+    const closeModal = () => {
+      modal.style.display = "none";
+      document.body.style.overflow = "";
     };
 
-        triggers.forEach(item => {
-            item.addEventListener('click', (e) => {
-                if (e.target) {
-                    e.preventDefault();
-                }
+    triggers.forEach((btns) => {
+      btns.addEventListener("click", (e) => {
+        if (e.target) {
+          e.preventDefault();
+        }
 
-                modal.style.display = "block";
-                document.body.style.overflow = "hidden";
-            });
-        });
+        modal.style.display = "block";
+        document.body.style.overflow = "hidden";
+      });
+    });
 
-        close.addEventListener('click', () => {
-            closeModal();
-        });
+    close.addEventListener("click", () => {
+      closeModal();
+    });
 
-        modal.addEventListener('click', (e) => {
-            if (e.target === modal) {
-                closeModal();
-            }
-        });
+    modal.addEventListener("click", (e) => {
+      if (e.target === modal) {
+        closeModal();
+      }
+    });
 
-        document.addEventListener('keydown', (e) => {
-            if (e.code === "Escape") {
-                closeModal();
-            }
-        });
-    };
+    document.addEventListener("keydown", (e) => {
+      if (e.code === "Escape") {
+        closeModal();
+      }
+    });
+  };
 
-    let showModalByTime = (selector, time) => {
-        setTimeout(function() {
-            document.querySelector(selector).style.display = "block";
-            document.body.style.overflow = "";
-        }, time);
-    };
+  let showModalByTime = (selector, time) => {
+    setTimeout(function () {
+      document.querySelector(selector).style.display = "block";
+      document.body.style.overflow = "";
+    }, time);
+  };
 
-    bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
-    bindModal('.phone_link', '.popup', '.popup .popup_close');
-    showModalByTime('.popup', 6000);
+  bindModal(
+    ".popup_engineer_btn",
+    ".popup_engineer",
+    ".popup_engineer .popup_close"
+  );
+  bindModal(".phone_link", ".popup", ".popup .popup_close");
+  showModalByTime(".popup", 6000);
 };
 
 export default modals;
