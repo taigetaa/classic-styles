824a81d0cbaa8e6d60ccc220fa6c378ccbe65fe5
Victoria <bethmorgendorffer@mail.ru>
2023-05-17 21:24:17 +0300

    half of tabs

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,10 @@
 import "./slider";
 import modals from './modules/modals';
+import tabs from './modules/tabs';
+import { table } from "console";
 
 window.addEventListener('DOMContentLoaded', () => {
     modals();
+    tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
+    //tabs();
 });
