b7af6806bc0f9ff3870c90170ceb93d2fea05f2b
Victoria <bethmorgendorffer@mail.ru>
2023-05-20 00:30:14 +0300

    tabs

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,9 @@
 import "./slider";
 import modals from './modules/modals';
 import tabs from './modules/tabs';
-import { table } from "console";
 
 window.addEventListener('DOMContentLoaded', () => {
     modals();
     tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
-    //tabs();
+    tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
 });
--- src/js/modules/tabs.js
+++ src/js/modules/tabs.js
@@ -4,17 +4,18 @@ const tabs = (headerSelector, tabSelector, contentSelector, activeClass) => {
   const content = document.querySelectorAll(contentSelector);
 
   const hideTabContent = () => {
-    content.forEach((item) => {
-      item.style.display = "none";
+    content.forEach(item => {
+      item.style.display = 'none';
     });
 
-    tab.forEach((item) => {
+    tab.forEach(item => {
       item.classList.remove(activeClass);
     });
   };
 
-  const showTabContent = (i) => {
-    content[i].style.display = "block";
+
+  const showTabContent = (i = 0) => {
+    content[i].style.display = 'block';
     tab[i].classList.add(activeClass);
   };
 
@@ -24,9 +25,8 @@ const tabs = (headerSelector, tabSelector, contentSelector, activeClass) => {
   header.addEventListener("click", (e) => {
     const target = e.target;
     if (
-      target &&
-      (target.classList.contains(tabSelector.replace(/\./, "")) ||
-        target.parentNode.classList.contains(tabSelector.replace(/\./, "")))
+      target.classList.contains(tabSelector.replace(/\./, "")) ||
+      target.parentNode.classList.contains(tabSelector.replace(/\./, ""))
     ) {
       tab.forEach((item, i) => {
         if (target == item || target.parentNode == item) {
@@ -38,4 +38,4 @@ const tabs = (headerSelector, tabSelector, contentSelector, activeClass) => {
   });
 };
 
-export default tabs();
+export default tabs;
