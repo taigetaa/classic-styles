72c299659c338be784167c8d33125d2c10e294d8
Victoria <bethmorgendorffer@mail.ru>
2023-05-24 21:24:20 +0300

    forms

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,22 @@
 import "./slider";
-import modals from './modules/modals';
-import initTabs from './modules/initTabs';
-import initForms from './modules/forms';
+import modals from "./modules/modals";
+import initTabs from "./modules/initTabs";
+import initForms from "./modules/forms";
 
-window.addEventListener('DOMContentLoaded', () => {
-    "use strict";
-    modals();
-    initTabs({
-        headerSelector: '.glazing_slider',
-        tabSelector: '.glazing_block',
-        contentSelector: '.glazing_content',
-        activeClass: 'active'
-    });
-    initTabs({
-        headerSelector: '.decoration_slider',
-        tabSelector: '.no_click',
-        contentSelector: '.decoration_content > div > div',
-        activeClass: 'after_click'
-    });
-    initForms();
+window.addEventListener("DOMContentLoaded", () => {
+  "use strict";
+  modals();
+  initTabs({
+    headerSelector: ".glazing_slider",
+    tabSelector: ".glazing_block",
+    contentSelector: ".glazing_content",
+    activeClass: "active",
+  });
+  initTabs({
+    headerSelector: ".decoration_slider",
+    tabSelector: ".no_click",
+    contentSelector: ".decoration_content > div > div",
+    activeClass: "after_click",
+  });
+  initForms();
 });
