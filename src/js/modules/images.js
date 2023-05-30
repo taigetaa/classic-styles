7eb89200275b5a5c7b3fb2b92d90828345a74afd
Victoria <bethmorgendorffer@mail.ru>
2023-05-30 14:36:57 +0300

    work images

--- src/js/modules/images.js
+++ src/js/modules/images.js
@@ -15,7 +15,7 @@ const images = () => {
   workSection.addEventListener("click", (e) => {
     e.preventDefault();
 
-    let target = e.target;
+    const target = e.target;
 
     if (target && target.classList.contains("preview")) {
       imgPopup.style.display = "flex";
